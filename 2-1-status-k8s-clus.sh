c22a13243c28b36a9879974c67ff9371bd09735d
Lorenzo Galelli <lorencegalelli@gmail.com>
2019-07-25 09:41:49 +0000

    Add files via upload
    Phase 2 Beta

---
+++ 2-1-status-k8s-clus.sh
@@ -0,0 +1,10 @@
+k8sID=$(cat /root/managed-k8s/log/create-k8s-cluster.log | jq '.id'| tr -d \")
+url="https://api.ionos.com/cloudapi/v5/k8s/"$k8sID
+
+curl -s --request GET \
+     -H 'Authorization: Bearer <APIKey>' \
+     --header "Content-Type: application/json" \
+"$url" > /root/managed-k8s/log/create-cluster-status.log
+status=$(cat /root/managed-k8s/log/create-cluster-status.log | jq '.metadata.state'| tr -d \")
+
+echo The current state of the Cluster is "$status"
